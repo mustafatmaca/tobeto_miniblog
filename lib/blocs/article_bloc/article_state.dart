b4501b6dc156b5d1c464320d51f193e129cf6cad
mustafatmaca <mustafaatmaca362@gmail.com>
2023-12-29 18:35:32 +0300

    bloc pattern

--- lib/blocs/article_bloc/article_state.dart
+++ lib/blocs/article_bloc/article_state.dart
@@ -4,22 +4,12 @@ abstract class ArticleState {}
 
 class ArticleInitial extends ArticleState {}
 
-class ArticlesLoading extends ArticleState {}
+class ArticleLoading extends ArticleState {}
 
 class ArticlesLoaded extends ArticleState {
   final List<Blog> blogs;
 
   ArticlesLoaded({required this.blogs});
-}
-
-class ArticlesError extends ArticleState {}
-
-class ArticleLoading extends ArticleState {}
-
-class ArticleLoaded extends ArticleState {
-  final Blog blog;
-
-  ArticleLoaded({required this.blog});
 }
 
 class ArticleError extends ArticleState {}
